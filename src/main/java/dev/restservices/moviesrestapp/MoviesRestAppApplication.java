125aae7837612f02c8031ff32f93907ef4897da0
Lukas <lookash85@hotmail.co.uk>
2018-04-24 10:52:02 +0200

    imported starter project to Eclipse

---
+++ src/main/java/dev/restservices/moviesrestapp/MoviesRestAppApplication.java
@@ -0,0 +1,12 @@
+package dev.restservices.moviesrestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesRestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesRestAppApplication.class, args);
+	}
+}
